b2cfb3a8378c1659e676f8e0a808bde71e189dc6
devCristoff <20222118@itla.edu.do>
2024-07-28 12:48:37 -0400

    [NEW]: Add generate random number helper

---
+++ fedex/Fedex.Core.Application/Helpers/RandomNumberHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace Fedex.Core.Application.Helpers
+{
+    public class RandomNumberHelper
+    {
+        private static readonly Random _random = new Random();
+
+        public static double GetRandomNumber(double minValue = 100, double maxValue = 2000)
+        {
+            double randomValue = _random.NextDouble() * (maxValue - minValue) + minValue;
+
+            return Math.Round(randomValue, 2);
+        }
+    }
+}
