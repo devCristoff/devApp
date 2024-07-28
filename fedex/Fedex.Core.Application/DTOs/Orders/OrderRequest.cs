73a26b32492a7377dd9121a4de4ba144b8c4598b
devCristoff <20222118@itla.edu.do>
2024-07-28 12:30:23 -0400

    [NEW]: Add order DTOs

---
+++ fedex/Fedex.Core.Application/DTOs/Orders/OrderRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Fedex.Core.Application.DTOs.Order
+{
+    public class OrderRequest
+    {
+        public string ContactAddress { get; set; }
+        public string WarehouseAddress { get; set; }
+        public List<string> PackageDimensions { get; set; }
+    }
+}
