2ea8e6278e6dceebfd2e88e8fcd58c2c58c3bdab
devCristoff <20222118@itla.edu.do>
2024-07-28 14:08:18 -0400

    [NEW]: Add account controller

---
+++ fedex/Fedex.Presentation.Api/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Fedex.Core.Application.DTOs.Account;
+using Fedex.Core.Application.Interfaces.Services;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Net.Mime;
+
+namespace Fedex.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [SwaggerTag("Sistema de membresía")]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        [HttpPost("authenticate")]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [SwaggerOperation(
+            Summary = "Login de usuario",
+            Description = "Autentica un usuario en el sistema y le retorna un JWT"
+        )]
+        public async Task<IActionResult> AuthenticateAsync(AuthenticationRequest request)
+        {
+            return Ok(await _accountService.AuthenticateAsync(request));
+        }
+    }
+}
