b96e1aee14f049f2c0801cfb737f02e4143be9a9
devCristoff <20222118@itla.edu.do>
2024-07-28 14:35:50 -0400

    [NEW]: Add api extension (swagger, erro handler middleware)

--- fedex/Fedex.Presentation.Api/Program.cs
+++ fedex/Fedex.Presentation.Api/Program.cs
@@ -1,12 +1,25 @@
 using Fedex.Core.Application;
 using Fedex.Infrastructure.Identity;
+using Fedex.WebApi.Extensions;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddIdentityInfrastructure(builder.Configuration);
 builder.Services.AddApplicationLayer();
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add(new ProducesAttribute("application/json"));
+}).ConfigureApiBehaviorOptions(options =>
+{
+    options.SuppressInferBindingSourcesForParameters = true;
+    options.SuppressMapClientErrors = true;
+});
+builder.Services.AddHealthChecks();
+builder.Services.AddSwaggerExtension();
+builder.Services.AddApiVersioningExtension();
+builder.Services.AddDistributedMemoryCache();
 // Dependency Injections
 builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>(); // IHttpContextAccesor
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -23,10 +36,20 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
+app.UseSwagggerExtension();
+app.UseErrorHandlerMiddleware();
+app.UseHealthChecks("/health");
+app.UseSession();
 
 app.MapControllers();
 
