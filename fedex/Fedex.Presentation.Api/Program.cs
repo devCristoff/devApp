511d2fefecb2634043ebfd864ac8550994fddf47
devCristoff <20222118@itla.edu.do>
2024-07-28 14:18:53 -0400

    [INFO]: Add identity configuration extentions to program class

--- fedex/Fedex.Presentation.Api/Program.cs
+++ fedex/Fedex.Presentation.Api/Program.cs
@@ -1,13 +1,19 @@
+using Fedex.Infrastructure.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddIdentityInfrastructure(builder.Configuration);
 builder.Services.AddControllers();
+// Dependency Injections
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>(); // IHttpContextAccesor
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+await app.Services.AddIdentitySeeds();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
