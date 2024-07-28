f15e2112cd4d2655b50fcb3937cb59f52b2ff123
devCristoff <20222118@itla.edu.do>
2024-07-28 12:08:05 -0400

    [NEW]: Add identity seeds & roles

---
+++ fedex/Fedex.Infrastructure.Identity/Seeds/DefaultPartnerUser.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Fedex.Core.Application.Enums;
+using Fedex.Infrastructure.Identity.Entities;
+
+namespace Fedex.Infrastructure.Identity.Seeds
+{
+    public static class DefaulPartnerUser
+    {
+        public static async Task SeedAsync(UserManager<ApplicationUser> userManager)
+        {
+            ApplicationUser defaultUser = new()
+            {
+                Id = "34796422-cda8-4aa2-bc8a-cdc567efae06",
+                UserName = "agentuser",
+                Email = "agentuser@email.com",
+                FirstName = "Jules",
+                LastName = "Doe",
+                EmailConfirmed = true,
+                PhoneNumberConfirmed = true,
+            };
+
+            if (userManager.Users.All(u => u.Id != defaultUser.Id))
+            {
+                var user = await userManager.FindByEmailAsync(defaultUser.Email);
+
+                if (user == null)
+                {
+                    await userManager.CreateAsync(defaultUser, "123P4$$w0rd!");
+                    await userManager.AddToRoleAsync(defaultUser, Roles.Partner.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ fedex/Fedex.Infrastructure.Identity/Seeds/DefaultRoles.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Fedex.Core.Application.Enums;
+
+namespace Fedex.Infrastructure.Identity.Seeds
+{
+    public static class DefaultRoles
+    {
+        public static async Task SeedAsync(RoleManager<IdentityRole> roleManager)
+        {
+            await roleManager.CreateAsync(new IdentityRole(Roles.SuperAdmin.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Roles.Admin.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Roles.Partner.ToString()));
+        }
+    }
+}
