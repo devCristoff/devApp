4f90ac5e4c17541f1de6b5f3c0765ab1f98266bd
devCristoff <20222118@itla.edu.do>
2024-07-28 16:02:29 -0400

    [INFO]: Remove order request DTO

--- fedex/Fedex.Core.Application/DTOs/Orders/OrderRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Fedex.Core.Application.DTOs.Order
-{
-    public class OrderRequest
-    {
-        public string ContactAddress { get; set; }
-        public string WarehouseAddress { get; set; }
-        public List<string> PackageDimensions { get; set; }
-    }
-}
