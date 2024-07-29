1887a1745ab6a9b44a6897446a800797eb586077
devCristoff <20222118@itla.edu.do>
2024-07-28 21:53:30 -0400

    [NEW]: Add order controller

---
+++ Domex/Domex.Presentation.Api/Controllers/v1/OrderController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Net.Mime;
+using Fedex.Core.Application.Features.Orders.Commands.CreateOrder;
+using Fedex.Core.Application.DTOs.Orders;
+
+namespace Fedex.WebApi.Controllers.v1
+{
+    [ApiVersion("1.0")]
+    [Authorize(Roles = "Partner")]
+    [SwaggerTag("Orders")]
+    public class OrderController : BaseApiController
+    {
+        public OrderController() {}
+
+        [HttpPost]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(OrderResponse))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [SwaggerOperation(
+            Summary = "Order creation",
+            Description = "Receive the necessary parameters to create a new order"
+        )]
+        public async Task<IActionResult> Post([FromBody] CreateOrderCommand command)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            return Ok(await Mediator.Send(command));
+        }
+    }
+}
