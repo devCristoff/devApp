f39369074621118b6170fbd1aa5a1559ff94043f
devCristoff <20222118@itla.edu.do>
2024-07-28 21:49:32 -0400

    [NEW]: Add CQRS feature for orders

---
+++ Domex/Domex.Core.Application/Features/Orders/Commands/CreateOrder/CreateOrderCommand.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Fedex.Core.Application.DTOs.Orders;
+using Fedex.Core.Application.Helpers;
+
+namespace Fedex.Core.Application.Features.Orders.Commands.CreateOrder
+{
+    /// <summary>
+    /// Parameters for creating an order
+    /// </summary>  
+    public class CreateOrderCommand : IRequest<OrderResponse>
+    {
+        public string Consigee { get; set; }
+        public string Consignor { get; set; }
+        public List<string> Cartons { get; set; }
+    }
+
+    public class CreateOrderCommandHandler : IRequestHandler<CreateOrderCommand, OrderResponse>
+    {
+        public CreateOrderCommandHandler()
+        {
+        }
+
+        public async Task<OrderResponse> Handle(CreateOrderCommand command, CancellationToken cancellationToken)
+        {
+            var orderResponse = new OrderResponse();
+            orderResponse.Amount = RandomNumberHelper.GetRandomNumber(command.Cartons.Count);
+            return orderResponse;
+        }
+    }
+}
