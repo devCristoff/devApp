6f7ece385423d3df015cf42718d1ace59ba61e42
devCristoff <20222118@itla.edu.do>
2024-07-29 18:53:55 -0400

    [NEW]: Add identity service registration

--- Vimenpaq/Vimenpaq.Presentation.Api/Program.cs
+++ Vimenpaq/Vimenpaq.Presentation.Api/Program.cs
@@ -1,7 +1,9 @@
+using Domex.Infrastructure.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddIdentityInfrastructure(builder.Configuration);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
