38a7660f565279a0040be9a60a8068fb9f9fb6ed
devCristoff <20222118@itla.edu.do>
2024-07-28 21:50:52 -0400

    [NEW]: Add error middleware handler

---
+++ Domex/Domex.Presentation.Api/Middlewares/ErrorHandlerMiddleware.cs
@@ -0,0 +1,63 @@
+﻿using Fedex.Core.Application.Exceptions;
+using Fedex.Core.Application.Wrappers;
+using System.Net;
+using System.Text.Json;
+
+namespace Fedex.WebApi.Middlewares
+{
+    public class ErrorHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ErrorHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception error)
+            {
+                var response = httpContext.Response;
+                response.ContentType = "application/json";
+
+                var responseModel = new Response<string>()
+                {
+                    Succeeded = false,
+                    Message = error?.Message
+                };
+                
+                switch (error)
+                {
+                    case ApiException e:
+                        switch (e.ErrorCode)
+                        {
+                            case (int)HttpStatusCode.BadRequest:
+                                response.StatusCode = (int)HttpStatusCode.BadRequest;
+                                break;
+                            case (int)HttpStatusCode.NotFound:
+                                response.StatusCode = (int)HttpStatusCode.NotFound;
+                                break;
+                            default:
+                                response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                                break;
+                        }
+                        break;
+                    case KeyNotFoundException e:
+                        response.StatusCode = (int)HttpStatusCode.NotFound;
+                        break;
+                    default: 
+                        response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        break;
+                }
+
+                var result = JsonSerializer.Serialize(responseModel);
+                await response.WriteAsync(result);
+            }
+        }
+    }
+}
