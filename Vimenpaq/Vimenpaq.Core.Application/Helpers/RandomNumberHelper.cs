4be309c8630c523d7264b15df0961cb761bd0319
devCristoff <20222118@itla.edu.do>
2024-07-30 00:22:03 -0400

    [NEW]: Add generate random number helper

---
+++ Vimenpaq/Vimenpaq.Core.Application/Helpers/RandomNumberHelper.cs
@@ -0,0 +1,17 @@
+﻿namespace Domex.Core.Application.Helpers
+{
+    public class RandomNumberHelper
+    {
+        private static readonly Random _random = new Random();
+
+        public static double GetRandomNumber(int numOrder)
+        {
+            double minValue = 100;
+            double maxValue = numOrder * 1000;
+
+            double randomValue = _random.NextDouble() * (maxValue - minValue) + minValue;
+
+            return Math.Round(randomValue, 2);
+        }
+    }
+}
