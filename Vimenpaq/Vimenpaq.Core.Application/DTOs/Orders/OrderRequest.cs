e1083a2dd90a01db2a76d2445bfe98bfaf71df4c
devCristoff <20222118@itla.edu.do>
2024-07-30 00:05:24 -0400

    [NEW]: Add order DTOs

---
+++ Vimenpaq/Vimenpaq.Core.Application/DTOs/Orders/OrderRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Vimenpaq.Core.Application.DTOs.Orders
+{
+    public class OrderRequest
+    {
+        public string Consigee { get; set; }
+        public string Consignor { get; set; }
+        public List<string> Cartons { get; set; }
+    }
+}
