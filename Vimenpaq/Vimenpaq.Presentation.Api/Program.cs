46fd15d02e9256001219cff39221da4489df0dab
devCristoff <20222118@itla.edu.do>
2024-07-30 00:26:22 -0400

    [NEW]: Add application layer registration

--- Vimenpaq/Vimenpaq.Presentation.Api/Program.cs
+++ Vimenpaq/Vimenpaq.Presentation.Api/Program.cs
@@ -1,9 +1,11 @@
+using Domex.Core.Application;
 using Domex.Infrastructure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddIdentityInfrastructure(builder.Configuration);
+builder.Services.AddApplicationLayer();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
