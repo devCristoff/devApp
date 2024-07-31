f14b38b4168a679069b8712bb2334e9dcde40ed9
devCristoff <20222118@itla.edu.do>
2024-07-30 23:55:42 -0400

    [NEW]: Add api extension (swagger, erro handler middleware)

--- Vimenpaq/Vimenpaq.Presentation.Api/Program.cs
+++ Vimenpaq/Vimenpaq.Presentation.Api/Program.cs
@@ -30,6 +30,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+app.UseSwagggerExtension();
+app.UseErrorHandlerMiddleware();
 
 app.MapControllers();
 
