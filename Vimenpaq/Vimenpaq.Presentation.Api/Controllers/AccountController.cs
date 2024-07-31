b2ac9c97504c3bb2150054919df4e9a1a8c05105
devCristoff <20222118@itla.edu.do>
2024-07-31 19:38:26 -0400

    [NEW]: Add account controller

---
+++ Vimenpaq/Vimenpaq.Presentation.Api/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vimenpaq.Core.Application.DTOs.Account;
+using Vimenpaq.Core.Application.Interfaces.Services;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Net.Mime;
+
+namespace Vimenpaq.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [SwaggerTag("Authentication")]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        [HttpPost("authenticate")]
+        [Consumes(MediaTypeNames.Application.Xml)]
+        [Produces(MediaTypeNames.Application.Xml)]
+        [SwaggerOperation(
+            Summary = "User Login",
+            Description = "Authenticates a user in the system and returns a JWT"
+        )]
+        public async Task<IActionResult> AuthenticateAsync([FromBody] AuthenticationRequest request)
+        {
+            return Ok(await _accountService.AuthenticateAsync(request));
+        }
+    }
+}
