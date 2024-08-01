60012e056e622637477917492dcff56e89e5b480
devCristoff <20222118@itla.edu.do>
2024-08-01 01:17:02 -0400

    [INFO]: Add XML config for properties

--- Vimenpaq/Vimenpaq.Core.Application/DTOs/Account/AuthenticationRequest.cs
+++ Vimenpaq/Vimenpaq.Core.Application/DTOs/Account/AuthenticationRequest.cs
@@ -1,16 +1,20 @@
 ﻿using Swashbuckle.AspNetCore.Annotations;
+using System.Xml.Serialization;
 
 namespace Vimenpaq.Core.Application.DTOs.Account
 {
     /// <summary>
     /// Parameters to perform user authentication
     /// </summary> 
+    [XmlRoot("AuthenticationRequest")]
     public class AuthenticationRequest
     {
         [SwaggerParameter(Description = "Email of the user to be logged in ")]
+        [XmlElement("email")]
         public string Email { get; set; }
 
         [SwaggerParameter(Description = "Password of the user to be logged in ")]
+        [XmlElement("password")]
         public string Password { get; set; }
     }
 }
