5f1737ce5a94d050ba8e896788d2b9b98e688ac3
devCristoff <20222118@itla.edu.do>
2024-08-01 01:24:26 -0400

    [NEW]: Add order controller feature

---
+++ Vimenpaq/Vimenpaq.Presentation.Api/Controllers/v1/OrderController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Net.Mime;
+using Vimenpaq.Core.Application.DTOs.Orders;
+using Vimenpaq.Core.Application.Interfaces.Services;
+
+namespace Vimenpaq.WebApi.Controllers.v1
+{
+    [ApiVersion("1.0")]
+    [Authorize(Roles = "Partner")]
+    [SwaggerTag("Orders")]
+    public class OrderController : BaseApiController
+    {
+        private readonly IOrderService _orderService;
+
+        public OrderController(IOrderService orderService) 
+        {
+            _orderService = orderService;
+        }
+
+        [HttpPost]
+        [Consumes(MediaTypeNames.Application.Xml)]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(OrderResponse))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [SwaggerOperation(
+            Summary = "Order creation",
+            Description = "Receive the necessary parameters to create a new order"
+        )]
+        public async Task<IActionResult> Post([FromBody] OrderRequest order)
+        {
+            if (!ModelState.IsValid || order.Packages.Count == 0)
+            {
+                return BadRequest();
+            }
+
+            return Ok(await _orderService.Create(order));
+        }
+    }
+}
