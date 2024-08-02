03f3ab645c68c3cafbd5716d630048abd1ffd8ad
devCristoff <20222118@itla.edu.do>
2024-08-02 16:15:53 -0400

    [NEW]: Add vimenpaq order service

--- cristoff/controllers/OfferController.js
+++ cristoff/controllers/OfferController.js
@@ -1,19 +1,29 @@
 const FedexService = require('../services/FedexService');
 const DomexService = require('../services/FedexService');
-const VimenpaqService = require('../services/FedexService');
+const VimenpaqService = require('../services/VimenpaqService');
 
 exports.PostBestOffer = async (req, res, next) => {    
     try {
-        const { contactAddress, warehouseAddress, packageDimensions } = req.body;
+        const { originAddress, destinyAddress, packageDimensions } = req.body;
 
-        const fedexOfferResponse = await FedexService.CreateOrder(req.fedexToken, contactAddress, warehouseAddress, packageDimensions);
+        const fedexOfferResponse = await FedexService.CreateOrder(req.fedexToken, originAddress, destinyAddress, packageDimensions);
+        const fedexData = await fedexOfferResponse.json()
+        
+        const vimenpaqOfferResponse = await VimenpaqService.CreateOrder(req.vimenpaqToken, originAddress, destinyAddress, packageDimensions);
+        const vimenpaqData = await vimenpaqOfferResponse.ResponseOfOrderResponse;
+        
+        const offers = [
+            { offer: fedexData.data.total, company: 'Fedex'  },
+            { offer: vimenpaqData.Data[0].Quote[0], company: 'Vimenpaq' },
+        ];
+        
+        const bestOffer = offers.reduce((minOffer, currentOffer) => {
+            return currentOffer.offer < minOffer.offer ? currentOffer : minOffer;
+        });
 
-        const data = await fedexOfferResponse.json()
-        console.log(data.data.total);
-
-        res.status(200).json(fedexOffer.json());
+        res.status(200).json(bestOffer);
     } catch (error) {
-        // console.error(error);
+        console.error(error);
         res.status(500).json({ error: 'Error fetching offers' });
     }
 }
