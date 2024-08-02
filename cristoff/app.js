7658a37ef6df54f4f70437ef2d6003d3a38fc6be
devCristoff <20222118@itla.edu.do>
2024-08-02 14:51:48 -0400

    [INFO]: Clean code for app.js

--- cristoff/app.js
+++ cristoff/app.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const middleware = require("./middlewares");
 
 const indexRouter = require("./routes/index");
 
 const app = express();
 app.set('port', 8080); // Set port
 
+// Middlewares
+app.use(middleware);
+
 // Route config
-app.use(indexRouter);
+app.use('/api', indexRouter);
 
 app.listen(app.get('port'), () => 
     console.log(`Server is running on port ${app.get('port')}`)
