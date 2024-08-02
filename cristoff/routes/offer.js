8a8c518baaf2fca88ff8d941049d395924922fcb
devCristoff <20222118@itla.edu.do>
2024-08-02 14:49:56 -0400

    [INFO]: update method and route for offer

--- cristoff/routes/offer.js
+++ cristoff/routes/offer.js
@@ -3,6 +3,6 @@ const express = require('express');
 const OfferController = require('../controllers/OfferController');
 const router = express.Router();
 
-router.get('/', OfferController.GetBest);
+router.post('/best-offer', OfferController.PostBestOffer);
 
 module.exports = router;
